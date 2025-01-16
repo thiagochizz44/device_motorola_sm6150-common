f8919e3aee6d5774dcf880f2c884f570e3d127ab
Thiago <diasthiago@protonmail.com>
2025-01-16 20:25:04 -0300

    Revert "sm6150-common: enable LOW_ART config for all variants"
    not needed we have props for this
    
    This reverts commit b0ddf5a6717a02c580ca23a98680eb38e4a5ca6e.

--- init/init_sm6150.cpp
+++ init/init_sm6150.cpp
@@ -65,7 +65,6 @@ void check_device()
         heaptargetutilization = "0.5";
         heapminfree = "8m";
         heapmaxfree = "32m";
-        SetProperty("ro.config.art_lowmem", "true");
     } else if (sys.totalram > 3072ull * 1024 * 1024) {
         // from - phone-xxhdpi-4096-dalvik-heap.mk
         heapstartsize = "8m";
@@ -74,7 +73,6 @@ void check_device()
         heaptargetutilization = "0.6";
         heapminfree = "8m";
         heapmaxfree = "16m";
-        SetProperty("ro.config.art_lowmem", "true");
     } else {
         // from - phone-xhdpi-2048-dalvik-heap.mk
         heapstartsize = "8m";
@@ -83,7 +81,6 @@ void check_device()
         heaptargetutilization = "0.75";
         heapminfree = "512k";
         heapmaxfree = "8m";
-        SetProperty("ro.config.art_lowmem", "true");
     }
 }
 
