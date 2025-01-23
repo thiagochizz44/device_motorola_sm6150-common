1681b27e8c3bae32a33901ee546f787a56b16db7
Thiago <diasthiago@protonmail.com>
2025-01-23 15:31:29 -0300

    fixup

--- common.mk
+++ common.mk
@@ -249,9 +249,6 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 # Reduce SystemServer Debug
 PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
 
-# Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-
 # Mobile data
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.mobiledata=false
